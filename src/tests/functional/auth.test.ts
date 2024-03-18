4b3ec05b2f7d25828cbddaa37d17e818e5c45b88
Ricardo Navarro <r.navarro@blackbullmarkets.com>
2024-03-17 22:08:51 -0600

    fixed tests

--- src/tests/functional/auth.test.ts
+++ src/tests/functional/auth.test.ts
@@ -9,15 +9,15 @@ describe("Testing auth endpoint", () => {
 		// Connect to the testing database before running the tests
 		//await sequelize.sync({ force: true }); // Sync models with the database and drop existing tables
 		// Seed the testing database with initial data
-		await User.create({
-			email: "test@test.com",
-			username: "test",
-			password: "password",
-			first_name: "John",
-			last_name: "Doe",
-			birthday: new Date("1990-01-01"),
-			user_type: "admin",
-		});
+		// await User.create({
+		// 	email: "test@test.com",
+		// 	username: "test",
+		// 	password: "password",
+		// 	first_name: "John",
+		// 	last_name: "Doe",
+		// 	birthday: new Date("1990-01-01"),
+		// 	user_type: "admin",
+		// });
 	});
 
 	afterAll(async () => {
@@ -27,6 +27,15 @@ describe("Testing auth endpoint", () => {
 
 	test("The healthcheck works", async () => {
 		const response = await request(server).get("/api/healthcheck");
+		const userQuery = await User.findOne({ where: { email: "test@test.com" } });
+		const user = userQuery?.dataValues;
+		expect(user).toBeDefined(); // Assert that the user record exists
+		expect(user?.username).toBe("test");
+		expect(user?.password).toBe("password");
+		expect(user?.first_name).toBe("John");
+		expect(user?.last_name).toBe("Doe");
+		expect(user?.birthday.toISOString()).toBe("1990-01-01T00:00:00.000Z");
+		expect(user?.user_type).toBe("admin");
 		expect(response.status).toBe(200);
 		expect(response.body).toEqual({
 			message: "Server is running",
