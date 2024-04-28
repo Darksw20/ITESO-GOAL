5f0b940c47d9f9270856e8f075d51aa470be18ae
Santiago-gluthe <94256359+Santiago0192@users.noreply.github.com>
2024-04-27 20:19:52 -0600

    TeamUser service

--- src/controllers/TeamController.ts
+++ src/controllers/TeamController.ts
@@ -1,5 +1,7 @@
 import { Request, Response } from "express";
 import Team from "../services/TeamService";
+import User from "../services/UserService";
+import TeamUser from "../services/TeamUserService";
 
 export default {
 	async create(req: Request, res: Response) {
@@ -82,12 +84,97 @@ export default {
 		}
 	},
 	async getMembers(req: Request, res: Response) {
-		res.send("get team members");
+		try {
+			const teamId = Number(req.params.id);
+			const team = await Team.find(teamId);
+	
+			if (!team) {
+				return res.status(404).send("Team not found");
+			}
+	
+			const members = await TeamUser.find(undefined,teamId);
+	
+			return res.status(200).json(members);
+		} catch (error) {
+			console.error("Error fetching team members:", error);
+			return res.status(500).send("Internal server error");
+		}
 	},
 	async addMembers(req: Request, res: Response) {
-		res.send("add team members");
+		const teamId = Number(req.params.id);
+		const userIds: number[] = req.body.members;
+
+		if (!teamId || !userIds || !Array.isArray(userIds) || userIds.length === 0) {
+			if (!teamId){
+				return res.status(400).json({ message: "Team Id is requiered" });
+			}
+			if (!userIds){
+				return res.status(400).json({ message: "User Ids are requiered" });
+			}
+			if (!Array.isArray(userIds) || userIds.length === 0){
+				return res.status(400).json({ message: "User Ids are requiered" });
+			}
+		} try {
+			const team = await Team.find(teamId);
+			if (!team) {
+				return res.status(404).send("Team not found");
+			}
+			const promises = userIds.map(async (userId) => {
+				const user = await User.find(userId);
+				if (!user) {
+					return res.status(404).send(`User with ID ${userId} not found`);
+				}
+
+				await TeamUser.create(
+					userId,
+					teamId
+				);
+			});
+			await Promise.all(promises);
+
+			return res.status(200).json({ message: "Members added successfully" });
+		} catch (err: any) {
+			console.log(err);
+			return res.status(500).json({ message: err.message });
+		}
 	},
 	async deleteMember(req: Request, res: Response) {
-		res.send("delete team member");
+		const teamId = Number(req.params.id);
+		const userIds: number[] = req.body.members;
+
+		if (!teamId || !userIds || !Array.isArray(userIds) || userIds.length === 0) {
+			if (!teamId){
+				return res.status(400).json({ message: "Team Id is requiered" });
+			}
+			if (!userIds){
+				return res.status(400).json({ message: "User Ids are requiered" });
+			}
+			if (!Array.isArray(userIds) || userIds.length === 0){
+				return res.status(400).json({ message: "User Ids are requiered" });
+			}
+		} try {
+			const team = await Team.find(teamId);
+			if (!team) {
+				return res.status(404).send("Team not found");
+			}
+			const promises = userIds.map(async (userId) => {
+				const user = await User.find(userId);
+				if (!user) {
+					return res.status(404).send(`User with ID ${userId} not found`);
+				}
+
+				const userTeam = await TeamUser.delete(
+					userId,
+					teamId
+				);
+				return res.status(200).json({ message: userTeam });
+			});
+			await Promise.all(promises);
+
+			return res.status(200).json({ message: "Members added successfully" });
+		} catch (err: any) {
+			console.log(err);
+			return res.status(500).json({ message: err.message });
+		}
 	},
 };
---
+++ src/services/TeamUserService.ts
@@ -0,0 +1,79 @@
+import UserTeam from "../models/UserTeam";
+
+export default {
+	create: async (
+		userId: number, teamId: number
+	) => {
+		try {
+			const userTeam = await UserTeam.create({
+				fk_user: userId,
+				fk_team: teamId
+			});
+
+			return {
+				id: userTeam.id,
+				userTeam: userTeam,
+			};
+		} catch (e) {
+			console.error(e);
+			return {
+				error: "User not registered to team",
+			};
+		}
+	},
+	find: async (userId?: number, teamId?: number, id?: number) => {
+		try {
+			if (id) {
+				const userTeam = await UserTeam.findByPk(id);
+				if (!userTeam) {
+					return { error: "UserTeam not found" };
+				}
+				return { userTeam: userTeam };
+			}
+	
+			if (userId && teamId) {
+				const userTeam = await UserTeam.findAll({ where: { fk_user: userId, fk_team: teamId } });
+				if (userTeam.length === 0) {
+					return { error: "No results for the specified user and team" };
+				}
+				return { userTeam: userTeam };
+			}
+	
+			if (userId) {
+				const userTeam = await UserTeam.findAll({ where: { fk_user: userId } });
+				if (userTeam.length === 0) {
+					return { error: "No teams found for the specified user" };
+				}
+				return { userTeam: userTeam };
+			}
+	
+			if (teamId) {
+				const userTeam = await UserTeam.findAll({ where: { fk_team: teamId } });
+				if (userTeam.length === 0) {
+					return { error: "No user found for the specified team" };
+				}
+				return { userTeam: userTeam };
+			}
+	
+			return { userTeams: await UserTeam.findAll() };
+		} catch (error) {
+			console.error("Error fetching user team:", error);
+			return { error: "Internal server error" };
+		}
+	},	
+	delete: async (userId: number, teamId: number) => {
+		try {
+
+			await UserTeam.destroy({ where: { fk_user: userId, fk_team: teamId } });
+
+			return {
+				message: "UserTeam deleted",
+			};
+		} catch (e) {
+			console.error(e);
+			return {
+				error: "UserTeam not deleted",
+			};
+		}
+	},
+};
